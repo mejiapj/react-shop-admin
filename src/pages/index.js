2ad928d25402609a49f6f660eb8a874beab71caf
mejiapj <mejiapj@gmail.com>
2023-09-24 16:24:31 -0500

    Creando la lógica del componente login

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
@@ -63,7 +63,7 @@ export default function Home() {
                   ))}
                 </div>
                 <div className="py-6">
-                  <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                  <a href="/login" className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
                   </a>
                 </div>
