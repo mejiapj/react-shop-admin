ad5d611bef79a14e07d8d23defafea4b63ebcbba
mejiapj <mejiapj@gmail.com>
2023-09-25 01:32:12 -0500

    16. Obteniendo la lista de productos desde la API

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -3,7 +3,7 @@ const VERSION = process.env.NEXT_PUBLIC_API_URL_VERSION;
 
 const endPoints = {
   products: {
-    getProducts: `${API}/api/${VERSION}/products/`,
+    getProducts: (limit, offset) => `${API}/api/${VERSION}/products?limit=${limit}&offset=${offset}`,
     postProducts: `${API}/api/${VERSION}/products/`,
     getProduct: (id) => `${API}/api/${VERSION}/products/${id}`,
     putProducts: (id) => `${API}/api/${VERSION}/products/${id}`,
